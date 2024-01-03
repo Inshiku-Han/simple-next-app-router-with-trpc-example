bfade1c88d7e6c920f6c3fb778c9a21900d4c18d
Inshiku-Han <inshiku.han@wise-vine.com>
2024-01-03 16:53:53 +0900

    style: format

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,7 +5,7 @@ import { httpBatchLink } from '@trpc/react-query'
 import { useState, type PropsWithChildren } from 'react'
 import { trpc } from '~/app/_trpc/client'
 
-export default function Providers({ children }: PropsWithChildren) {
+export function Providers({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() =>
     trpc.createClient({
